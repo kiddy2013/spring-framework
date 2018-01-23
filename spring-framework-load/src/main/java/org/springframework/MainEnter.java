58eb1ab3ca4bf371cf53855079aa915132c0b5a4
jack <duqiang.dq@alibaba-inc.com>
2018-01-23 11:26:29 +0800

    增加spring-load的module

---
+++ spring-framework-load/src/main/java/org/springframework/MainEnter.java
@@ -0,0 +1,11 @@
+package org.springframework;
+
+/**
+ * @author jack
+ */
+public class MainEnter {
+
+	public static void main(String[] args) {
+		System.out.println("hello spring framework");
+	}
+}
